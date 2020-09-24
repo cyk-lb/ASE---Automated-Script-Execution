33461e380fd96a2921c7c00e21ef85631e89e408
Baur <lukas.baur@ipa.fraunhofer.de>
2020-09-24 18:04:51 +0200

    advanced python script

--- script.py
+++ script.py
@@ -1,16 +1,39 @@
 # this is a normal python script. Include any logic here.
-# imports
+# -------------- imports ----------------------------------
 import datetime
+import urllib.request, json
 
-# logic
+# -------------- constants ----------------------------------
+FILEPATH = ''
+FILENAME = 'loaded_data'
+FILE_ENDING = '.json'
+
+# TODO: replace with your API
+MY_API = 'http://maps.googleapis.com/maps/api/geocode/json?address=google'
+
+
+# -------------- logic ----------------------------------
 def get_current_timestamp():
-    return datetime.datetime.now()
+    time_stamp =  str(datetime.datetime.now())
+    time_stamp =  time_stamp.replace(' ', '_')
+    return time_stamp.replace(':', '_')
 
-def append_to_file(filepath, content):
-    with open("testfile.txt", "a") as fh:
+def write_to_file(filepath, filename, file_ending, content):
+    filename = '{path}{name}_{time}{ending}'.format(path=filepath, name=filename, time=get_current_timestamp(), ending=file_ending)
+    with open(filename, "a") as fh:
         fh.write(content)
 
-FILEPATH = 'testfile.txt'
+def load_data_from_remote_server(url):
+    # load plain data
+    with urllib.request.urlopen(url) as url_handler:
+        response = url_handler.read().decode()
+        # parse to python objects
+        data = json.loads(response)
+    return data
 
-timestamp = get_current_timestamp()
-append_to_file(FILEPATH, '{} some important message\n'.format(timestamp))
+
+
+data = load_data_from_remote_server(MY_API)
+print('  {} data entries loaded.'.format(len(data)))
+plain_data = str(data)
+write_to_file(FILEPATH, FILENAME, FILE_ENDING, plain_data)
